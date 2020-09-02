557da2b51bc6c561c671718bd076d39f2b813cb2
HabiAyman <49348764+HabiAyman@users.noreply.github.com>
2020-09-02 17:27:48 +0200

    Update and rename app.p to app.py

--- app.py
+++ app.py
@@ -102,27 +102,12 @@ def venues():
         "num_upcoming_shows": len(db.session.query(Show).filter(Show.venue_id==1).filter(Show.start_time>datetime.now()).all())
       })
       
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
+ data.append({
+      "city": area.city,
+      "state": area.state, 
+      "venues": venue_data
+    })
+    
   return render_template('pages/venues.html', areas=data);
 
 @app.route('/venues/search', methods=['POST'])
