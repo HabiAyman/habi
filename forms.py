d6c0e1bdf209e4ab37ffab92a5ce32ec168fb7a2
HabiAyman <49348764+HabiAyman@users.noreply.github.com>
2020-09-04 01:05:12 +0200

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,89 +1,8 @@
 from datetime import datetime
 from flask_wtf import Form
-from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, TextAreaField, BooleanField
-from wtforms.validators import DataRequired, AnyOf, URL, Regexp, ValidationError
-import re
-
-
-def validate_phone(form, field):
-    if not re.search(r"^[0-9]*$", field.data):
-        raise ValidationError("Phone number should only contain digits.")
-
-
-genres_choices = [
-    ('Alternative', 'Alternative'),
-    ('Blues', 'Blues'),
-    ('Classical', 'Classical'),
-    ('Country', 'Country'),
-    ('Electronic', 'Electronic'),
-    ('Folk', 'Folk'),
-    ('Funk', 'Funk'),
-    ('Hip-Hop', 'Hip-Hop'),
-    ('Heavy Metal', 'Heavy Metal'),
-    ('Instrumental', 'Instrumental'),
-    ('Jazz', 'Jazz'),
-    ('Musical Theatre', 'Musical Theatre'),
-    ('Pop', 'Pop'),
-    ('Punk', 'Punk'),
-    ('R&B', 'R&B'),
-    ('Reggae', 'Reggae'),
-    ('Rock n Roll', 'Rock n Roll'),
-    ('Soul', 'Soul'),
-    ('Other', 'Other'),
-]
-state_choices = [
-    ('AL', 'AL'),
-    ('AK', 'AK'),
-    ('AZ', 'AZ'),
-    ('AR', 'AR'),
-    ('CA', 'CA'),
-    ('CO', 'CO'),
-    ('CT', 'CT'),
-    ('DE', 'DE'),
-    ('DC', 'DC'),
-    ('FL', 'FL'),
-    ('GA', 'GA'),
-    ('HI', 'HI'),
-    ('ID', 'ID'),
-    ('IL', 'IL'),
-    ('IN', 'IN'),
-    ('IA', 'IA'),
-    ('KS', 'KS'),
-    ('KY', 'KY'),
-    ('LA', 'LA'),
-    ('ME', 'ME'),
-    ('MT', 'MT'),
-    ('NE', 'NE'),
-    ('NV', 'NV'),
-    ('NH', 'NH'),
-    ('NJ', 'NJ'),
-    ('NM', 'NM'),
-    ('NY', 'NY'),
-    ('NC', 'NC'),
-    ('ND', 'ND'),
-    ('OH', 'OH'),
-    ('OK', 'OK'),
-    ('OR', 'OR'),
-    ('MD', 'MD'),
-    ('MA', 'MA'),
-    ('MI', 'MI'),
-    ('MN', 'MN'),
-    ('MS', 'MS'),
-    ('MO', 'MO'),
-    ('PA', 'PA'),
-    ('RI', 'RI'),
-    ('SC', 'SC'),
-    ('SD', 'SD'),
-    ('TN', 'TN'),
-    ('TX', 'TX'),
-    ('UT', 'UT'),
-    ('VT', 'VT'),
-    ('VA', 'VA'),
-    ('WA', 'WA'),
-    ('WV', 'WV'),
-    ('WI', 'WI'),
-    ('WY', 'WY'),
-]
+from wtforms.widgets import TextArea
+from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
+from wtforms.validators import DataRequired, AnyOf, URL
 
 
 class ShowForm(Form):
@@ -109,33 +28,105 @@ class VenueForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=state_choices
+        choices=[
+            ('AL', 'AL'),
+            ('AK', 'AK'),
+            ('AZ', 'AZ'),
+            ('AR', 'AR'),
+            ('CA', 'CA'),
+            ('CO', 'CO'),
+            ('CT', 'CT'),
+            ('DE', 'DE'),
+            ('DC', 'DC'),
+            ('FL', 'FL'),
+            ('GA', 'GA'),
+            ('HI', 'HI'),
+            ('ID', 'ID'),
+            ('IL', 'IL'),
+            ('IN', 'IN'),
+            ('IA', 'IA'),
+            ('KS', 'KS'),
+            ('KY', 'KY'),
+            ('LA', 'LA'),
+            ('ME', 'ME'),
+            ('MT', 'MT'),
+            ('NE', 'NE'),
+            ('NV', 'NV'),
+            ('NH', 'NH'),
+            ('NJ', 'NJ'),
+            ('NM', 'NM'),
+            ('NY', 'NY'),
+            ('NC', 'NC'),
+            ('ND', 'ND'),
+            ('OH', 'OH'),
+            ('OK', 'OK'),
+            ('OR', 'OR'),
+            ('MD', 'MD'),
+            ('MA', 'MA'),
+            ('MI', 'MI'),
+            ('MN', 'MN'),
+            ('MS', 'MS'),
+            ('MO', 'MO'),
+            ('PA', 'PA'),
+            ('RI', 'RI'),
+            ('SC', 'SC'),
+            ('SD', 'SD'),
+            ('TN', 'TN'),
+            ('TX', 'TX'),
+            ('UT', 'UT'),
+            ('VT', 'VT'),
+            ('VA', 'VA'),
+            ('WA', 'WA'),
+            ('WV', 'WV'),
+            ('WI', 'WI'),
+            ('WY', 'WY'),
+        ]
     )
     address = StringField(
         'address', validators=[DataRequired()]
     )
     phone = StringField(
-        'phone', validators=[validate_phone]
+        'phone'
     )
     image_link = StringField(
         'image_link'
     )
     genres = SelectMultipleField(
+        # TODO implement enum restriction
         'genres', validators=[DataRequired()],
-        choices=genres_choices
-    )
-    facebook_link = StringField(
-        'facebook_link', validators=[URL()]
+        choices=[
+            ('Alternative', 'Alternative'),
+            ('Blues', 'Blues'),
+            ('Classical', 'Classical'),
+            ('Country', 'Country'),
+            ('Electronic', 'Electronic'),
+            ('Folk', 'Folk'),
+            ('Funk', 'Funk'),
+            ('Hip-Hop', 'Hip-Hop'),
+            ('Heavy Metal', 'Heavy Metal'),
+            ('Instrumental', 'Instrumental'),
+            ('Jazz', 'Jazz'),
+            ('Musical Theatre', 'Musical Theatre'),
+            ('Pop', 'Pop'),
+            ('Punk', 'Punk'),
+            ('R&B', 'R&B'),
+            ('Reggae', 'Reggae'),
+            ('Rock n Roll', 'Rock n Roll'),
+            ('Soul', 'Soul'),
+            ('Other', 'Other'),
+        ]
     )
     website = StringField(
         'website', validators=[URL()]
     )
+    facebook_link = StringField(
+        'facebook_link', validators=[URL()]
+    )
     seeking_talent = BooleanField(
-        'seeking_talent'
+        'seeking_talent',
     )
     seeking_description = StringField(
-        'seeking_description'
-
+        'seeking_description', widget=TextArea()
     )
 
 
@@ -148,28 +139,102 @@ class ArtistForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=state_choices
+        choices=[
+            ('AL', 'AL'),
+            ('AK', 'AK'),
+            ('AZ', 'AZ'),
+            ('AR', 'AR'),
+            ('CA', 'CA'),
+            ('CO', 'CO'),
+            ('CT', 'CT'),
+            ('DE', 'DE'),
+            ('DC', 'DC'),
+            ('FL', 'FL'),
+            ('GA', 'GA'),
+            ('HI', 'HI'),
+            ('ID', 'ID'),
+            ('IL', 'IL'),
+            ('IN', 'IN'),
+            ('IA', 'IA'),
+            ('KS', 'KS'),
+            ('KY', 'KY'),
+            ('LA', 'LA'),
+            ('ME', 'ME'),
+            ('MT', 'MT'),
+            ('NE', 'NE'),
+            ('NV', 'NV'),
+            ('NH', 'NH'),
+            ('NJ', 'NJ'),
+            ('NM', 'NM'),
+            ('NY', 'NY'),
+            ('NC', 'NC'),
+            ('ND', 'ND'),
+            ('OH', 'OH'),
+            ('OK', 'OK'),
+            ('OR', 'OR'),
+            ('MD', 'MD'),
+            ('MA', 'MA'),
+            ('MI', 'MI'),
+            ('MN', 'MN'),
+            ('MS', 'MS'),
+            ('MO', 'MO'),
+            ('PA', 'PA'),
+            ('RI', 'RI'),
+            ('SC', 'SC'),
+            ('SD', 'SD'),
+            ('TN', 'TN'),
+            ('TX', 'TX'),
+            ('UT', 'UT'),
+            ('VT', 'VT'),
+            ('VA', 'VA'),
+            ('WA', 'WA'),
+            ('WV', 'WV'),
+            ('WI', 'WI'),
+            ('WY', 'WY'),
+        ]
     )
-
     phone = StringField(
-        'phone', validators=[DataRequired(), Regexp("^[0-9]*$", message="Phone number should only contain digits")]
+        # TODO implement validation logic for state
+        'phone'
     )
-
     image_link = StringField(
         'image_link'
     )
     genres = SelectMultipleField(
+        # TODO implement enum restriction
         'genres', validators=[DataRequired()],
-        choices=genres_choices
+        choices=[
+            ('Alternative', 'Alternative'),
+            ('Blues', 'Blues'),
+            ('Classical', 'Classical'),
+            ('Country', 'Country'),
+            ('Electronic', 'Electronic'),
+            ('Folk', 'Folk'),
+            ('Funk', 'Funk'),
+            ('Hip-Hop', 'Hip-Hop'),
+            ('Heavy Metal', 'Heavy Metal'),
+            ('Instrumental', 'Instrumental'),
+            ('Jazz', 'Jazz'),
+            ('Musical Theatre', 'Musical Theatre'),
+            ('Pop', 'Pop'),
+            ('Punk', 'Punk'),
+            ('R&B', 'R&B'),
+            ('Reggae', 'Reggae'),
+            ('Rock n Roll', 'Rock n Roll'),
+            ('Soul', 'Soul'),
+            ('Other', 'Other'),
+        ]
     )
     facebook_link = StringField(
+        # TODO implement enum restriction
         'facebook_link', validators=[URL()]
     )
-
     website = StringField(
-        'website',  validators=[URL()]
+        'website', validators=[URL()]
     )
-
-    seeking_venue = BooleanField('seeking_venue')
-
-    seeking_description = StringField('seeking_description')
+    seeking_venue = BooleanField(
+        'seeking_venue',
+    )
+    seeking_description = StringField(
+        'seeking_description', widget=TextArea()
+    )
